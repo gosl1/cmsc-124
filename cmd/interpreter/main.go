6e522f8e0f2f39e40c3d3cb5bc70a8c531925654
Ethan Sean Gapulan <ethangapulan@gmail.com>
2026-09-23 22:21:53 +0800

    feat: moved token types and token structure into separate file

--- cmd/interpreter/main.go
+++ cmd/interpreter/main.go
@@ -3,30 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
-	"unicode"
 )
-
-// --- TOKEN TYPES & STRUCT ---
-
-type TokenType string
-
-const (
-	TokenTypeVAR        TokenType = "VAR"
-	TokenTypeIDENTIFIER TokenType = "IDENTIFIER"
-	TokenTypeEQUAL      TokenType = "EQUAL"
-	TokenTypeSTRING     TokenType = "STRING"
-	TokenTypePRINT      TokenType = "PRINT"
-	TokenTypeEOF        TokenType = "EOF"
-)
-
-type Token struct {
-	Type    TokenType
-	Lexeme  string
-	Literal any
-	Line    int
-}
-
-
 
 func main() {
 	args := os.Args[1:]
@@ -35,19 +12,20 @@ func main() {
 		runFileTokenize(args[1])
 		return
 	}
+
+	runREPL()
 }
 
 func runFileTokenize(path string) {
 	source, err := os.ReadFile(path)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "lab1: cannot read '%s': %v\n", path, err)
+		fmt.Fprintf(os.Stderr, "ama: cannot read '%s': %v\n", path, err)
 		os.Exit(65)
 	}
 
-	scanner := NewScanner(string(source)) //Have yet to create the function
-	tokens, hasError := scanner.ScanTokens() //Have to make this too
+	scanner := NewScanner(string(source))
+	tokens, hasError := scanner.ScanTokens()
 
-	// Exit code 65 on error with clean stdout
 	if hasError {
 		os.Exit(65)
 	}
